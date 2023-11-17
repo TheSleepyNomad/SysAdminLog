aee694b5bf1603e49dcbb35415133ad28ec5079b
Ilya Pankov <iluxaan@mail.ru>
2023-11-17 11:57:53 +0300

    feat: add new commands and handler

--- app/handlers/message_handler.py
+++ app/handlers/message_handler.py
@@ -5,6 +5,21 @@ async def start_command_handler(msg: Message):
     await msg.answer('Админ вернулся!')
 
 
+async def mycar_command_handler(msg: Message):
+    await msg.answer('Раздел "Мои машины"')
 
+
+async def tasks_command_handler(msg: Message):
+    await msg.answer('Раздел "Мои задачи"')
+
+
+async def profile_command_handler(msg: Message):
+    await msg.answer('Личный раздел')
+
+
+async def admin_command_handler(msg: Message):
+    await msg.answer('Раздел администрирования')
+
+    
 def register_command_handler(dp):
     dp.message.register(start_command_handler, Command("start"))
